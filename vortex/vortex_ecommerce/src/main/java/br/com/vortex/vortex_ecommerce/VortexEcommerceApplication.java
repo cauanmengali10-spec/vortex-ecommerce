f163892298b28a785847be9d8829ab702e7c1823
Cauan Mengali <cauanmengali10@gmail.com>
2026-09-12 19:19:30 -0300

    commit inicial

---
+++ vortex/vortex_ecommerce/src/main/java/br/com/vortex/vortex_ecommerce/VortexEcommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.vortex.vortex_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VortexEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VortexEcommerceApplication.class, args);
+	}
+
+}
